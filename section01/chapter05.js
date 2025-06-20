5adc3790fa4139cf88ca89c8b05d780895f20aed
kaeuhy <20212105@edu.hanbat.ac.kr>
2025-06-20 19:15:20 +0900

    자료형

---
+++ section01/chapter05.js
@@ -0,0 +1,21 @@
+let num1 = 27;
+let num2 = 10;
+
+
+let inf = Infinity;
+let mInf = -Infinity;
+
+let nam = NaN;
+
+
+let myName = "강은현";
+let myLocation = "멋지다";
+let introduce = myName + myLocation;
+
+let introduceText = `${myName}은 ${myLocation}에 거주합니다`;
+
+let isSwithchOn = true;
+let isEmpty = false;
+
+let empty = null;
+
