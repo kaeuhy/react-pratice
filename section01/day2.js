c1bcaaf247a8504c6f36e987d6dad7a878230475
kaeuhy <20212105@edu.hanbat.ac.kr>
2025-06-27 12:23:59 +0900

    day2 quiz

--- section01/day2.js
+++ section01/day2.js
@@ -1,3 +1,4 @@
+// quiz 01
 const x = 15;
 const y = 20;
 
@@ -5,3 +6,42 @@ let max = x > y ? x : y;
 
 console.log(max);
 
+// quiz 02
+
+const a = true;
+
+switch (typeof a) {
+    case "number": {
+        console.log("숫자");
+        break;
+    }
+    case "string": {
+        console.log("문자열");
+        break;
+    }
+    case "boolean": {
+        console.log("불리언");
+        break;
+    }
+    case "undefined": {
+        console.log("언디파인");
+        break;
+    }
+    case "object": {
+        console.log("객체");
+        break;
+    }
+}
+
+// quiz 03
+let temperature = 10;
+
+if (temperature < 0) {
+    console.log("매우 추움");
+} else if (0 < temperature < 10) {
+    console.log("추움");
+} else if (10 < temperature < 20) {
+    console.log("적당");
+} else if (temperature > 20) {
+    console.log("더움");
+}
